5d6a6bae83fb7e0763d2600a0e4df6e821163b5b
Arthur da Costa Farias <arthurdacosta22012007@gmail.com>
2026-09-10 07:45:36 -0300

    comentários dos códigos

--- aula_03_09/parte_1/parte_1.ino
+++ aula_03_09/parte_1/parte_1.ino
@@ -1,27 +1,33 @@
-int pinBotao = 2;
-int pinLD = 9;
-int val = 0;
+/////////////////////////////////////////////////////////
+// Código para controlar sentido e velocidade do motor //
+/////////////////////////////////////////////////////////
+
+int pinBotao = 2; // Pino onde do Arduino onde está o botão, que muda o sentido do motor
+int pinLD = 9; // Pino onde está o controle de velocidade do motor
+int val = 0; // Variável para guardar o valor do potenciômetro
 int IN1 = 3;
 int IN2 = 4;
+// IN1 e IN2 são variáveis para mudança do sentido do motor
 
 void setup() {
-  pinMode(pinBotao, INPUT);
+  pinMode(pinBotao, INPUT); // Configura botão como entrada
   pinMode(pinLD, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  // Configura saídas de dados 
   Serial.begin(9600);
 }
 
 void loop() {
-  val = analogRead(A0);
-  analogWrite(pinLD, val);
-  Serial.println(val);
-  int valorBotao = digitalRead(pinBotao);
-  Serial.println(valorBotao);
-  if(valorBotao == 0){
+  val = analogRead(A0); // Lê o valor do potenciômetro
+  analogWrite(pinLD, val); // Controla velocidade do motor de acordo com o potenciômetro
+  Serial.println(val); // Escreve o valor do potenciômetro no Serial Monitor
+  int valorBotao = digitalRead(pinBotao); // Lê se o botão foi apertado ou não
+  Serial.println(valorBotao); // Escreve se o botão foi pressionado ou não
+  if(valorBotao == 0){ // Se o botão não foi apertado o motor gira no sentido horário
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-  } else {
+  } else { // Se foi apertado gira no sentido anti-horário
     digitalWrite(IN2, LOW);
     digitalWrite(IN1, HIGH);
   }
--- aula_03_09/parte_2/parte_2.ino
+++ aula_03_09/parte_2/parte_2.ino
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////
+// Código para parte 2 com integração de LEDs //
+////////////////////////////////////////////////
+
 int pinBotao = 2;
 int pinLD = 9;
 int val = 0;
@@ -8,6 +12,7 @@ int pinLed_2 = 6;
 int pinLed_3 = 11;
 int pinLed_4 = 8;
 int pinLed_5 = 10;
+// Pinos dos 5 LEDs
 
 void setup() {
   pinMode(pinBotao, INPUT);
@@ -22,29 +27,33 @@ void loop() {
   analogWrite(pinLD, val);
   Serial.println(val/4);
   int valorBotao = digitalRead(pinBotao);
-  if(valorBotao == 0){
-    digitalWrite(pinLed, HIGH);
-    digitalWrite(pinLed_2, LOW);
-    digitalWrite(IN1, LOW);
+  if(valorBotao == 0){ // Se o botão não for apertado
+    digitalWrite(pinLed, HIGH); // LED liga
+    digitalWrite(pinLed_2, LOW); // LED apaga
+    digitalWrite(IN1, LOW); 
     digitalWrite(IN2, HIGH);
-  } else {
-    digitalWrite(pinLed, LOW);
-    digitalWrite(pinLed_2, HIGH);
+  } else { // Se o botão for apertado
+    digitalWrite(pinLed, LOW); // LED apaga 
+    digitalWrite(pinLed_2, HIGH); // LED liga
     digitalWrite(IN2, LOW);
     digitalWrite(IN1, HIGH);
   }
 
-  if(val/4 < 170){
-    digitalWrite(pinLed_5, HIGH);
+  if(val/4 < 170){ // Se o valor do potenciômetro < 170
+    digitalWrite(pinLed_5, HIGH); 
     digitalWrite(pinLed_4, LOW);
     digitalWrite(pinLed_3, LOW);
-  } else if(170 >= val/4 && val/4 <= 200){
+	// Somente 1 LED liga
+  } else if(170 >= val/4 && val/4 <= 200){ 
+  // Se o valor do potenciômetro estiver entre 170 e 200
     digitalWrite(pinLed_5, HIGH);
     digitalWrite(pinLed_4, HIGH);
     digitalWrite(pinLed_3, LOW);
-  } if(val/4 > 200) {
+  // 2 LEDs ligam
+  } if(val/4 > 200) { // Se o valor do potenciômetro > 200
     digitalWrite(pinLed_5, HIGH);
     digitalWrite(pinLed_4, HIGH);
     digitalWrite(pinLed_3, HIGH);
+	// Todos os LEDs ligam
   }
 }
